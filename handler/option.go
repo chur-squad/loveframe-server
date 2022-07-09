2827f5808ba006456e5ade33d74163c46db85afb
hongbin <fusis1@naver.com>
2022-07-09 18:22:19 +0900

    feat: 데이터베이스 설정 추가

--- handler/option.go
+++ handler/option.go
@@ -1,5 +1,6 @@
 package handler
 
+import "database/sql"
 
 // Option is an interface for dependency injection.
 type Option interface {
@@ -7,26 +8,27 @@ type Option interface {
 }
 
 type Config struct {
-	Mysql						string
-	CdnEndpoint					string
-	GroupSalt					string
-	UserSalt					string
+	Mysql       *sql.DB
+	CdnEndpoint string
+	GroupSalt   string
+	UserSalt    string
 }
 
 // OptionFunc is a function for Option interface.
 type OptionFunc func(h *Handler)
+
 // anonymous func, it has only input
-// 
+//
 
 func (o OptionFunc) apply(h *Handler) { o(h) }
 
-
 // WithConfig returns a function for setting config.
 func WithConfig(c *Config) OptionFunc {
-	return func(h *Handler) {h.Cfg = c }
+	return func(h *Handler) { h.Cfg = c }
 }
+
 //return handler setting function
-//annonymous func 
+//annonymous func
 
 /*
 func PrettyPrint(target ...interface{}) {
@@ -43,8 +45,8 @@ func PrettyPrint(target ...interface{}) {
 */
 // Valid checks to be correct config or not.
 func (cfg *Config) Valid() (ok bool) {
-	if cfg.CdnEndpoint == "" { 
-		// temporary remove mySQLDSN 
+	if cfg.CdnEndpoint == "" {
+		// temporary remove mySQLDSN
 		return
 	}
 	// Check a salt for encrypting data exists or not.
