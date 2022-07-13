f0ec9196d9d2b1dc130a31f09598027b1e190bae
jae-kimSeoul <seojaeinblank@gmail.com>
2022-07-13 14:27:52 +0900

    add : bash scripts for local running

---
+++ scripts/local_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export CDN_ENDPOINT=https://d5hyco5fyogxb.cloudfront.net
+export GROUP_CODE_SALT=group-code-salt-local
+export USER_CODE_SALT=user-code-salt-local
+export DATABASE_USERNAME=db-user-local
+export DATABASE_PASSWORD=db-password-local
+export DATABASE_HOST=localhost
+export DATABASE_PORT=3306
+export DATABASE_NAME=db-name-local
